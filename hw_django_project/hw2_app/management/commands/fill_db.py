14c416f46422c0d978412423ca1307eee65ecb88
My Name <Me@gmail.com>
2023-09-07 20:16:21 +0400

    filled db

--- hw_django_project/hw2_app/management/commands/fill_db.py
+++ hw_django_project/hw2_app/management/commands/fill_db.py
@@ -10,10 +10,9 @@ class Command(BaseCommand):
             client = Client(name=f'Name_{i}', email=f'mail{i}@mail.ru', phone_number=8906567 + i, adress=f'adress_{i}',
                             registration_date= datetime.datetime(2022, 3, 11))
             client.save()
+            orders = Order.objects.create(customer=client, total_price=120)
             for j in range (1, 6):
                 product = Product(name=f'name_{j}', description=f'description_{j}', price= j * 3, product_amount= j, 
                                   date= datetime.datetime(2022, 3, 11))
                 product.save()
-                for k in range (1, 6): 
-                    order = Order(customer = client, products = product, date_ordered=datetime.datetime(2023, random.randint(1, 12), random.randint(1, 30)))
-                    order.save()
+                orders.products.add(product)
