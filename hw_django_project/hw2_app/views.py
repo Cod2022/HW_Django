b43307a6c927b0fdd10a15b7456af115b35fc059
My Name <Me@gmail.com>
2023-09-10 09:02:45 +0400

    changed orders func

--- hw_django_project/hw2_app/views.py
+++ hw_django_project/hw2_app/views.py
@@ -3,8 +3,10 @@ from .models import Client, Product, Order
 
 def orders(request, client_id):
     client = get_object_or_404(Client, pk=client_id)
-    orders = Order.objects.filter(customer=client)
-    context = {'client': client, 'orders': orders}
+    # orders = Order.objects.filter(customer=client)
+    orders = Order.objects.filter(customer=client).prefetch_related('products')
+    products = [product for order in orders for product in order.products.all()]
+    context = {'client': client, 'orders': orders, 'products': products}
     return render(request, 'hw2_app/orders.html', context)
     
 
